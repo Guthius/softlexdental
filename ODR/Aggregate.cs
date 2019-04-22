79b4bdbb259ed51dd7ebc37e0c1d72ff47f6a3f0
Dennis Hummel <dennis@softlex.nl>
2019-04-22 08:07:57 +0300

    Cleaned up the ODR project.

--- ODR/Aggregate.cs
+++ ODR/Aggregate.cs
@@ -1,52 +1,59 @@
-using System;
-using System.Windows.Forms;
-using OpenDentBusiness;
+namespace ODR
+{
+    public class Aggregate
+    {
+        static decimal runningSum;
+        static string groupByVal;
 
-namespace ODR{
-	///<summary></summary>
-	public class Aggregate{
-		private static decimal runningSum;
-		private static string groupByVal;
+        public static string RunningSumForAccounts(object groupBy, object debitAmt, object creditAmt, object acctType)
+        {
+            if (debitAmt == null || creditAmt == null)
+            {
+                return 0.ToString("N");
+            }
+            try
+            {
+                // Cannot read debitAmt and creditAmt as decimals because it makes the general ledger detail report fail.
+                // Simply cast as decimals when doing mathematical operations.
+                double debit = (double)debitAmt;
+                double credit = (double)creditAmt;
 
-		///<summary></summary>
-		public static string RunningSum(string groupBy,string addValue){
-			decimal num=PIn.Decimal(addValue);
-			if(groupByVal==null || groupBy!=groupByVal){//if new or changed group
-				runningSum=0;
-			}
-			groupByVal=groupBy;
-			runningSum+=num;
-			return runningSum.ToString("F");
-		}
+                if (groupByVal == null || groupBy.ToString() != groupByVal) // If new or changed group
+                {
+                    runningSum = 0;
+                }
 
-		///<summary></summary>
-		public static string RunningSumForAccounts(object groupBy, object debitAmt, object creditAmt, object acctType) {
-			if(debitAmt==null || creditAmt==null){
-				return 0.ToString("N");
-			}
-			try {
-				//Cannot read debitAmt and creditAmt as decimals because it makes the general ledger detail report fail.  Simply cast as decimals when doing mathematical operations.
-				double debit=(double)debitAmt;//PIn.PDouble(debitAmt);
-				double credit=(double)creditAmt;//PIn.PDouble(creditAmt)
-				if(groupByVal==null || groupBy.ToString()!=groupByVal) {//if new or changed group
-					runningSum=0;
-				}
-				groupByVal=groupBy.ToString();
-				if(TestValue.AccountDebitIsPos(acctType.ToString())) {
-					runningSum+=(decimal)debit-(decimal)credit;
-				}
-				else {
-					runningSum+=(decimal)credit-(decimal)debit;
-				}
-				return runningSum.ToString("N");
-			}
-			catch {
-				return 0.ToString("N");
-			}
-		}
+                groupByVal = groupBy.ToString();
+                if (AccountDebitIsPos(acctType.ToString()))
+                {
+                    runningSum += (decimal)debit - (decimal)credit;
+                }
+                else
+                {
+                    runningSum += (decimal)credit - (decimal)debit;
+                }
+                return runningSum.ToString("N");
+            }
+            catch
+            {
+                return 0.ToString("N");
+            }
+        }
 
-	}
+        static bool AccountDebitIsPos(string accountType)
+        {
+            switch (accountType)
+            {
+                case "0": // asset
+                case "4": // expense
+                    return true;
 
-	
-
+                case "1": // liability
+                case "2": // equity // because liabilities and equity are treated the same
+                case "3": // revenue
+                    return false;
+            }
+            return true; // will never happen
+        }
+    }
 }
