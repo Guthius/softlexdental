7e012d3b37a3d9ea3d4590df7aadfb15a2bfcc0e
Dennis Hummel <dhummel@gmail.com>
2019-09-16 12:45:41 +0300

    Cleanup

--- OpenDental/Logic/FeeL.cs
+++ OpenDental/Logic/FeeL.cs
@@ -7,175 +7,174 @@ using System.Windows.Forms;
 
 namespace OpenDental
 {
-    ///<summary></summary>
-    public class FeeL {
+    public class FeeL
+    {
+        /// <summary>
+        /// Imports fees into the database from the provided file.
+        /// </summary>
+        /// <param name="fileName">
+        /// Must be a tab-delimited .xls or .csv file. Each row must have two columns. 
+        /// The first column must be the proc code and the second column must be the fee amount.
+        /// </param>
+        public static void ImportFees(long feeScheduleId, long clinicId, long providerId, string fileName, Form currentForm, bool showMessage = true)
+        {
+            currentForm.Cursor = Cursors.WaitCursor;
+            ODProgress.ShowAction(() => ImportFeesWorker(fileName, currentForm, feeScheduleId, clinicId, providerId), startingMessage: "Importing fees, please wait...");
+            currentForm.Cursor = Cursors.Default;
 
-		///<summary>Imports fees into the database from the provided file.</summary>
-		///<param name="fileName">Must be a tab-delimited .xls or .csv file. Each row must have two columns. The first column must be the proc code
-		///and the second column must be the fee amount.</param>
-		public static void ImportFees(long feeSchedNum,long clinicNum,long provNum,string fileName,Form currentForm,bool showMessage=true) {
-			currentForm.Cursor=Cursors.WaitCursor;
-			ODProgress.ShowAction(() => ImportFeesWorker(fileName,currentForm,feeSchedNum,clinicNum,provNum),
-				startingMessage:"Importing fees, please wait...");
-			currentForm.Cursor=Cursors.Default;
-			if(showMessage) {
-				MsgBox.Show(currentForm,"Fee schedule imported.");
-			}
-		}
+            if (showMessage)
+            {
+                MessageBox.Show(
+                    "Fee schedule imported.", 
+                    "Fees", 
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Information);
+            }
+        }
 
-		///<summary>Logic that imports fees. Runs on a background thread while the progress bar is up.</summary>
-		public static void ImportFeesWorker(string fileName,Form currentForm,long feeSchedNum,long clinicNum,long provNum) {
-			FeeCache feeCache=new FeeCache();
-			List<Fee> listNewFees=feeCache.GetListFees(feeSchedNum,clinicNum,provNum);
-			string[] fields;
-			double feeAmt;
-			using(StreamReader sr=new StreamReader(fileName)) {
-				string line=sr.ReadLine();
-				while(line!=null) {
-					fields=line.Split(new string[1] {"\t"},StringSplitOptions.None);
-					if(fields.Length>1){// && fields[1]!=""){//we no longer skip blank fees
-						if(fields[1]=="") {
-							feeAmt=-1;//triggers deletion of existing fee, but no insert.
-						}
-						else {
-							feeAmt=PIn.Double(fields[1]);
-						}
-						listNewFees=Fees.Import(fields[0],feeAmt,feeSchedNum,clinicNum,provNum,listNewFees);
-					}
-					line=sr.ReadLine();
-				}
-			}
-			feeCache.BeginTransaction();
-			feeCache.RemoveFees(feeSchedNum,clinicNum,provNum);
-			foreach(Fee fee in listNewFees) {
-				feeCache.Add(fee);
-			}
-			feeCache.SaveToDb();
-		}
+        /// <summary>
+        /// Logic that imports fees. Runs on a background thread while the progress bar is up.
+        /// </summary>
+        public static void ImportFeesWorker(string fileName, Form currentForm, long feeScheduleId, long clinicId, long providerId)
+        {
+            FeeCache feeCache = new FeeCache();
+            List<Fee> listNewFees = feeCache.GetListFees(feeScheduleId, clinicId, providerId);
+            string[] fields;
+            double feeAmt;
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                string line = sr.ReadLine();
+                while (line != null)
+                {
+                    fields = line.Split(new string[1] { "\t" }, StringSplitOptions.None);
+                    if (fields.Length > 1)
+                    {// && fields[1]!=""){//we no longer skip blank fees
+                        if (fields[1] == "")
+                        {
+                            feeAmt = -1;//triggers deletion of existing fee, but no insert.
+                        }
+                        else
+                        {
+                            feeAmt = PIn.Double(fields[1]);
+                        }
+                        listNewFees = Fees.Import(fields[0], feeAmt, feeScheduleId, clinicId, providerId, listNewFees);
+                    }
+                    line = sr.ReadLine();
+                }
+            }
+            feeCache.BeginTransaction();
+            feeCache.RemoveFees(feeScheduleId, clinicId, providerId);
+            foreach (Fee fee in listNewFees)
+            {
+                feeCache.Add(fee);
+            }
+            feeCache.SaveToDb();
+        }
 
-		///<summary>ImportFees and ImportFeesWorker methods above are deprecated.  This is the replacement. Runs on a background thread while the progress bar is up.</summary>
-		public static void ImportFees2(string fileName,long feeSchedNum,long clinicNum,long provNum) {
-			List<Fee> listFees=Fees.GetListExact(feeSchedNum,clinicNum,provNum);
-			string[] fields;
-			int counter=0;
-			int lineCount=File.ReadAllLines(fileName).Length;//quick and dirty
-			using(StreamReader sr=new StreamReader(fileName)) {
-				string line=sr.ReadLine();
-				while(line!=null) {
-					fields=line.Split(new string[1] {"\t"},StringSplitOptions.None);
-					if(fields.Length<2){// && fields[1]!=""){//we no longer skip blank fees
-						line=sr.ReadLine();
-						continue;
-					}
-					long codeNum = ProcedureCodes.GetCodeNum(fields[0]);
-					if(codeNum==0){
-						line=sr.ReadLine();
-						continue;
-					}
-					Fee fee=Fees.GetFee(codeNum,feeSchedNum,clinicNum,provNum,listFees);
-					string feeOldStr="";
-					DateTime datePrevious=DateTime.MinValue;
-					if(fee!=null) {
-						feeOldStr="Old Fee: "+fee.Amount.ToString("c")+", ";
-						datePrevious=fee.SecDateTEdit;
-					}
-					if(fields[1]=="") {//an empty entry will delete an existing fee, but not insert a blank override
-						if(fee==null){//nothing to do
-							//counter++;
-							//line=sr.ReadLine();
-							//continue;
-						}
-						else{
-							//doesn't matter if the existing fee is an override or not.
-							Fees.Delete(fee);
-							SecurityLog.Write(null, SecurityLogEvents.ProcFeeEdit,"Procedure: "+fields[0]+", "
-								+feeOldStr
-								//+", Deleted Fee: "+fee.Amount.ToString("c")+", "
-								+"Fee Schedule: "+FeeScheds.GetDescription(feeSchedNum)+". "
-								+"Fee deleted using the Import button in the Fee Tools window.",codeNum,
-								DateTime.MinValue);
-							SecurityLog.Write(null, SecurityLogEvents.LogFeeEdit,"Fee deleted",fee.FeeNum,datePrevious);
-						}
-					}
-					else {//value found
-						if(fee==null){//no current fee
-							fee=new Fee();
-							fee.Amount=PIn.Double(fields[1]);
-							fee.FeeSched=feeSchedNum;
-							fee.CodeNum=codeNum;
-							fee.ClinicNum=clinicNum;//Either 0 because you're importing on an HQ schedule or local clinic because the feesched is localizable.
-							fee.ProvNum=provNum;
-							Fees.Insert(fee);
-						}
-						else{
-							fee.Amount=PIn.Double(fields[1]);
-							Fees.Update(fee);
-						}
-						SecurityLog.Write(null, SecurityLogEvents.ProcFeeEdit,"Procedure: "+fields[0]+", "
-							+feeOldStr
-							+", New Fee: "+fee.Amount.ToString("c")+", "
-							+"Fee Schedule: "+FeeScheds.GetDescription(feeSchedNum)+". "
-							+"Fee changed using the Import button in the Fee Tools window.",codeNum,
-							DateTime.MinValue);
-						SecurityLog.Write(null, SecurityLogEvents.LogFeeEdit,"Fee changed",fee.FeeNum,datePrevious);
-					}
-					//FeeSchedEvent.Fire(ODEventType.FeeSched,new ProgressBarHelper("Importing fees...",));
-					double percent=(double)counter*100d/(double)lineCount;
-					counter++;
-					FeeSchedEvent.Fire(ODEventType.FeeSched,new ProgressBarHelper(
-						"Importing fees...",((int)percent).ToString(),blockValue:(int)percent,progressStyle:ProgBarStyle.Continuous));
-					line=sr.ReadLine();
-				}
-			}
-			
-		}
+        ///<summary>ImportFees and ImportFeesWorker methods above are deprecated.  This is the replacement. Runs on a background thread while the progress bar is up.</summary>
+        public static void ImportFees2(string fileName, long feeSchedNum, long clinicNum, long provNum)
+        {
+            List<Fee> listFees = Fees.GetListExact(feeSchedNum, clinicNum, provNum);
+            string[] fields;
+            int counter = 0;
+            int lineCount = File.ReadAllLines(fileName).Length;//quick and dirty
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                string line = sr.ReadLine();
+                while (line != null)
+                {
+                    fields = line.Split(new string[1] { "\t" }, StringSplitOptions.None);
+                    if (fields.Length < 2)
+                    {// && fields[1]!=""){//we no longer skip blank fees
+                        line = sr.ReadLine();
+                        continue;
+                    }
+                    long codeNum = ProcedureCodes.GetCodeNum(fields[0]);
+                    if (codeNum == 0)
+                    {
+                        line = sr.ReadLine();
+                        continue;
+                    }
+                    Fee fee = Fees.GetFee(codeNum, feeSchedNum, clinicNum, provNum, listFees);
+                    string feeOldStr = "";
+                    DateTime datePrevious = DateTime.MinValue;
+                    if (fee != null)
+                    {
+                        feeOldStr = "Old Fee: " + fee.Amount.ToString("c") + ", ";
+                        datePrevious = fee.SecDateTEdit;
+                    }
+                    if (fields[1] == "")
+                    {//an empty entry will delete an existing fee, but not insert a blank override
+                        if (fee == null)
+                        {//nothing to do
+                         //counter++;
+                         //line=sr.ReadLine();
+                         //continue;
+                        }
+                        else
+                        {
+                            Fees.Delete(fee);
 
-		///<summary>Returns true if current user can edit the given feeSched, otherwise false.
-		///Shows a MessageBox if user is not allowed to edit.</summary>
-		public static bool CanEditFee(FeeSched feeSched,long provNum,long clinicNum) {
-			string error;
-			if(!Fees.CanEditFee(feeSched,provNum,clinicNum,out error)) {
-				MessageBox.Show(error);
-				return false;
-			}
-			return true;
-		}
-		/* doesn't seem to be used from anywhere
-		///<summary>If the named fee schedule does not exist, then it will be created.  It always returns the defnum for the feesched used, regardless of whether it already existed.  procCode must have already been tested for valid code, and feeSchedName must not be blank.</summary>
-		public static long ImportTrojan(string procCode,double amt,string feeSchedName) {
-			FeeSched feeSched=FeeScheds.GetByExactName(feeSchedName);
-			//if isManaged, then this should be done differently from here on out.
-			if(feeSched==null){
-				//add the new fee schedule
-				feeSched=new FeeSched();
-				feeSched.ItemOrder=FeeSchedC.ListLong.Count;
-				feeSched.Description=feeSchedName;
-				feeSched.FeeSchedType=FeeScheduleType.Normal;
-				//feeSched.IsNew=true;
-				FeeScheds.Insert(feeSched);
-				//Cache.Refresh(InvalidType.FeeScheds);
-				//Fees.Refresh();
-				DataValid.SetInvalid(InvalidType.FeeScheds, InvalidType.Fees);
-			}
-			if(feeSched.IsHidden){
-				feeSched.IsHidden=false;//unhide it
-				FeeScheds.Update(feeSched);
-				DataValid.SetInvalid(InvalidType.FeeScheds);
-			}
-			Fee fee=Fees.GetFee(ProcedureCodes.GetCodeNum(procCode),feeSched.FeeSchedNum);
-			if(fee==null) {
-				fee=new Fee();
-				fee.Amount=amt;
-				fee.FeeSched=feeSched.FeeSchedNum;
-				fee.CodeNum=ProcedureCodes.GetCodeNum(procCode);
-				Fees.Insert(fee);
-			}
-			else{
-				fee.Amount=amt;
-				Fees.Update(fee);
-			}
-			return feeSched.FeeSchedNum;
-		}	*/
+                            SecurityLog.Write(null, SecurityLogEvents.ProcFeeEdit, 
+                                $"Procedure: {fields[0]}, {feeOldStr}, " +
+                                $"Fee Schedule: {FeeScheds.GetDescription(feeSchedNum)}. " +
+                                $"Fee deleted using the Import button in the Fee Tools window.", 
+                                codeNum, null);
 
-	}
+                            SecurityLog.Write(null, SecurityLogEvents.LogFeeEdit, "Fee deleted", fee.FeeNum, datePrevious);
+                        }
+                    }
+                    else
+                    {
+                        if (fee == null)
+                        {
+                            fee = new Fee
+                            {
+                                Amount = PIn.Double(fields[1]),
+                                FeeSched = feeSchedNum,
+                                CodeNum = codeNum,
+                                ClinicNum = clinicNum,
+                                ProvNum = provNum
+                            };
+                            Fees.Insert(fee);
+                        }
+                        else
+                        {
+                            fee.Amount = PIn.Double(fields[1]);
+                            Fees.Update(fee);
+                        }
+
+                        SecurityLog.Write(null, SecurityLogEvents.ProcFeeEdit, 
+                            $"Procedure: {fields[0]}, {feeOldStr}, New Fee: {fee.Amount.ToString("c")}, " +
+                            $"Fee Schedule: {FeeScheds.GetDescription(feeSchedNum)}. " +
+                            $"Fee changed using the Import button in the Fee Tools window.", 
+                            codeNum, null);
+
+                        SecurityLog.Write(null, SecurityLogEvents.LogFeeEdit, "Fee changed", fee.FeeNum, datePrevious);
+                    }
+                    //FeeSchedEvent.Fire(ODEventType.FeeSched,new ProgressBarHelper("Importing fees...",));
+                    double percent = (double)counter * 100d / (double)lineCount;
+                    counter++;
+                    FeeSchedEvent.Fire(ODEventType.FeeSched, new ProgressBarHelper(
+                        "Importing fees...", ((int)percent).ToString(), blockValue: (int)percent, progressStyle: ProgBarStyle.Continuous));
+                    line = sr.ReadLine();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns true if current user can edit the given feeSched, otherwise false.
+        /// Shows a MessageBox if user is not allowed to edit.
+        /// </summary>
+        public static bool CanEditFee(FeeSched feeSched, long provNum, long clinicNum)
+        {
+            if (!Fees.CanEditFee(feeSched, provNum, clinicNum, out var error))
+            {
+                MessageBox.Show(error);
+                return false;
+            }
+
+            return true;
+        }
+    }
 }
--- OpenDental/Logic/PrefL.cs
+++ OpenDental/Logic/PrefL.cs
@@ -19,6 +19,8 @@ namespace OpenDental
         /// </summary>
         public static void DownloadInstallPatchFromURI(string downloadUri, string destinationPath, bool runSetupAfterDownload, bool showShutdownWindow, string destinationPath2)
         {
+            DialogResult result;
+
             bool isShutdownWindowNeeded = showShutdownWindow;
 
             while (isShutdownWindowNeeded)
@@ -30,16 +32,18 @@ namespace OpenDental
                     {
                         //turn off signal reception for 5 seconds so this workstation will not shut down.
                         Signalods.SignalLastRefreshed = MiscData.GetNowDateTime().AddSeconds(5);
-                        Signalod sig = new Signalod();
-                        sig.IType = InvalidType.ShutDownNow;
-                        Signalods.Insert(sig);
-                        Computer.ClearAllHeartBeats(Environment.MachineName);//always assume success
+                        Signalods.Insert(new Signalod
+                        {
+                            IType = InvalidType.ShutDownNow
+                        });
+
+                        Computer.ClearAllHeartBeats(Environment.MachineName);
+
                         isShutdownWindowNeeded = false;
-                        //SecurityLog.Write(Permissions.Setup,0,"Shutdown all workstations.");//can't do this because sometimes no user.
                     }
                     else if (formShutdown.DialogResult == DialogResult.Cancel)
                     {
-                        var result =
+                        result =
                             MessageBox.Show(
                                 "Are you sure you want to cancel the update?", 
                                 "Update", 
@@ -68,42 +72,55 @@ namespace OpenDental
                 return;
             }
 
-            WebRequest wr = WebRequest.Create(downloadUri);
-            WebResponse webResp = null;
+            var webRequest = WebRequest.Create(downloadUri);
+            WebResponse webResponse;
             try
             {
-                webResp = wr.GetResponse();
+                webResponse = webRequest.GetResponse();
             }
-            catch (Exception ex)
+            catch (Exception exception)
             {
-                MsgBoxCopyPaste msgbox = new MsgBoxCopyPaste(ex.Message + "\r\nUri: " + downloadUri);
-
-                msgbox.ShowDialog();
+                using (var msgBoxCopyPaste = new MsgBoxCopyPaste(exception.Message + "\r\nUri: " + downloadUri))
+                {
+                    msgBoxCopyPaste.ShowDialog();
+                }
 
                 Preference.Update(PreferenceName.UpdateInProgressOnComputerName, "");
                 return;
             }
 
-            int fileSize = (int)webResp.ContentLength / 1024;
-            FormProgress FormP = new FormProgress();
+            int fileSize = (int)webResponse.ContentLength / 1024;
+            var formProgress = new FormProgress
+            {
+                MaxVal = (double)fileSize / 1024,
+                NumberMultiplication = 100,
+                DisplayText = "?currentVal MB of ?maxVal MB copied",
+                NumberFormat = "F"
+            };
+
             //start the thread that will perform the download
-            ThreadStart downloadDelegate = delegate { DownloadInstallPatchWorker(downloadUri, destinationPath, webResp.ContentLength, ref FormP); };
-            Thread workerThread = new Thread(downloadDelegate);
+
+            void downloadDelegate()
+            {
+                DownloadInstallPatchWorker(downloadUri, destinationPath, webResponse.ContentLength, ref formProgress);
+            }
+
+            var workerThread = new Thread(downloadDelegate);
+
             workerThread.Start();
-            //display the progress dialog to the user:
-            FormP.MaxVal = (double)fileSize / 1024;
-            FormP.NumberMultiplication = 100;
-            FormP.DisplayText = "?currentVal MB of ?maxVal MB copied";
-            FormP.NumberFormat = "F";
-            FormP.ShowDialog();
-            if (FormP.DialogResult == DialogResult.Cancel)
+            if (formProgress.ShowDialog() == DialogResult.Cancel)
             {
                 workerThread.Abort();
+
                 Preference.Update(PreferenceName.UpdateInProgressOnComputerName, "");
+
+                formProgress.Dispose();
+
                 return;
             }
+            formProgress.Dispose();
 
-            if (destinationPath2 != null && destinationPath2 != "")
+            if (!string.IsNullOrEmpty(destinationPath2))
             {
                 if (File.Exists(destinationPath2))
                 {
@@ -114,7 +131,9 @@ namespace OpenDental
                     catch (Exception ex)
                     {
                         FormFriendlyException.Show("Error deleting file:\r\n" + ex.Message, ex);
+
                         Preference.Update(PreferenceName.UpdateInProgressOnComputerName, "");
+
                         return;
                     }
                 }
@@ -123,12 +142,17 @@ namespace OpenDental
 
             if (!runSetupAfterDownload) return;
 
-            string msg = "Download succeeded. Setup program will now begin.  When done, restart the program on this computer, then on the other computers.";
+            result =
+                MessageBox.Show(
+                    "Download succeeded. Setup program will now begin. When done, restart the program on this computer, then on the other computers.", 
+                    "Update", 
+                    MessageBoxButtons.OKCancel,
+                    MessageBoxIcon.Question);
 
-            if (MessageBox.Show(msg, "", MessageBoxButtons.OKCancel) != DialogResult.OK)
+            if (result == DialogResult.Cancel)
             {
-                //Clicking cancel gives the user a chance to avoid running the setup program,
-                Preference.Update(PreferenceName.UpdateInProgressOnComputerName, "");//unlock workstations, since nothing was actually done.
+                Preference.Update(PreferenceName.UpdateInProgressOnComputerName, "");
+
                 return;
             }
 
@@ -150,39 +174,50 @@ namespace OpenDental
                 //Notify the user to go manually stop the services that could not automatically stop.
                 if (!string.IsNullOrEmpty(servicesNotStopped))
                 {
-                    MsgBoxCopyPaste msgBCP = new MsgBoxCopyPaste(
-                        "The following services could not be stopped.  You need to manually stop them before continuing.\r\n" + servicesNotStopped);
+                    var msgBoxCopyPaste = new MsgBoxCopyPaste(
+                        "The following services could not be stopped. You need to manually stop them before continuing.\r\n" + servicesNotStopped);
 
-                    msgBCP.ShowDialog();
+                    msgBoxCopyPaste.ShowDialog();
+                    msgBoxCopyPaste.Dispose();
                 }
             }
             #endregion
+
             try
             {
                 Process.Start(destinationPath);
+
                 FormOpenDental.S_ProcessKillCommand();
             }
             catch
             {
                 Preference.Update(PreferenceName.UpdateInProgressOnComputerName, "");
-                MsgBox.Show(FormP, "Could not launch setup");
+
+                MessageBox.Show(
+                    "Could not launch setup.",
+                    "Update", 
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Error);
             }
         }
 
-        ///<summary>This is the function that the worker thread uses to actually perform the download.
-        ///Can also call this method in the ordinary way if the file to be transferred is short.</summary>
+        /// <summary>
+        /// This is the function that the worker thread uses to actually perform the download. Can 
+        /// also call this method in the ordinary way if the file to be transferred is short.
+        /// </summary>
         private static void DownloadInstallPatchWorker(string downloadUri, string destinationPath, long contentLength, ref FormProgress progressIndicator)
         {
-            using (WebClient webClient = new WebClient())
-            using (Stream streamRead = webClient.OpenRead(downloadUri))
-            using (FileStream fileStream = new FileStream(destinationPath, FileMode.Create))
+            using (var webClient = new WebClient())
+            using (var stream = webClient.OpenRead(downloadUri))
+            using (var fileStream = new FileStream(destinationPath, FileMode.Create))
             {
                 int bytesRead;
                 long position = 0;
                 byte[] buffer = new byte[10 * 1024];
+
                 try
                 {
-                    while ((bytesRead = streamRead.Read(buffer, 0, buffer.Length)) > 0)
+                    while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
                     {
                         position += bytesRead;
                         if (position != contentLength)
@@ -192,16 +227,21 @@ namespace OpenDental
                         fileStream.Write(buffer, 0, bytesRead);
                     }
                 }
-                catch (Exception ex)
+                catch (Exception exception)
                 {
-                    //Set the error message so that the user can call in and complain and we can get more information about what went wrong.
-                    //This error message will NOT show if the user hit the Cancel button and a random exception happened (because the window will have closed).
-                    progressIndicator.ErrorMessage = ex.Message;
+                    // Set the error message so that the user can call in and complain and we can 
+                    // get more information about what went wrong. This error message will NOT show
+                    // if the user hit the Cancel button and a random exception happened (because 
+                    // the window will have closed).
+
+                    progressIndicator.ErrorMessage = exception.Message;
                 }
             }
 
-            //If the file was successfully downloaded, set the progress indicator to maximum so that it closes the progress window.
-            //Otherwise leave the window open so that the error message can be displayed to the user in red text.
+            // If the file was successfully downloaded, set the progress indicator to maximum so 
+            // that it closes the progress window. Otherwise leave the window open so that the 
+            // error message can be displayed to the user in red text.
+
             if (string.IsNullOrEmpty(progressIndicator.ErrorMessage))
             {
                 progressIndicator.CurrentVal = (double)contentLength / 1024;
