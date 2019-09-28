aa959aacfbd507ab2c49f5e56da4f6d5a81ff895
Dennis Hummel <dhummel@gmail.com>
2019-09-28 13:17:37 +0300

    Cleanup

--- OpenDentBusiness/DataRecord.cs
+++ OpenDentBusiness/DataRecord.cs
@@ -1,4 +1,20 @@
-﻿using MySql.Data.MySqlClient;
+﻿/**
+ * Copyright (C) 2019 Dental Stars SRL
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; If not, see <http://www.gnu.org/licenses/>
+ */
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 
@@ -37,9 +53,9 @@ namespace OpenDentBusiness
         /// <returns>A list of records.</returns>
         protected static List<T> SelectMany<T>(string commandText, DataRecordBuilder<T> dataRecordBuilder, params MySqlParameter[] parameters)
         {
-            if (dataRecordBuilder == null) throw new ArgumentNullException("dataRecordBuilder");
+            if (dataRecordBuilder == null) throw new ArgumentNullException(nameof(dataRecordBuilder));
 
-            List<T> records = new List<T>();
+            var records = new List<T>();
 
             DataConnection.Execute(connection =>
             {
@@ -75,7 +91,7 @@ namespace OpenDentBusiness
         /// <returns>A single record.</returns>
         public static T SelectOne<T>(string commandText, DataRecordBuilder<T> dataRecordBuilder, params MySqlParameter[] parameters)
         {
-            if (dataRecordBuilder == null) throw new ArgumentNullException("dataRecordBuilder");
+            if (dataRecordBuilder == null) throw new ArgumentNullException(nameof(dataRecordBuilder));
 
             return DataConnection.Execute(connection =>
             {
